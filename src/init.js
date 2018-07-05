a57239d690c8fd47e05cd7a7a399cb8cb7dae12a
Bo Chen <bochen303@gmail.com>
2018-07-05 12:38:46 +0700

    add init

---
+++ src/init.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+
+if (!fs.existsSync(path.join(__dirname, '../data/responsibility_events.json'))) {
+    fs.writeFileSync(path.join(__dirname, '../data/responsibility_events.json'), '[]');
+}
+
+if (!fs.existsSync(path.join(__dirname, '../data/task_events.json'))) {
+    fs.writeFileSync(path.join(__dirname, '../data/task_events.json'), '[]');
+}
