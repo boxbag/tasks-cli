0c70783b93cc552eda901fcd0fddcf30bc57e77d
Bo Chen <bochen303@gmail.com>
2018-07-12 18:45:47 +0700

    update plan to include recurring tasks

--- src/views/pending_tasks_on_date.js
+++ src/views/pending_tasks_on_date.js
@@ -6,5 +6,7 @@ const scoredTasks = require('./scored_tasks');
 module.exports = function (date) {
     return scoredTasks
         .filter(t => t.status === 'PENDING')
-        .filter(t => moment(date).startOf('day').toDate().getTime() == moment(t.start_date).startOf('day').toDate().getTime());
+        .filter(t => {
+            return moment(date).startOf('day').toDate().getTime() == moment(t.start_date).startOf('day').toDate().getTime() || (t.recurring_type === 'Weekly' && t.recurring_days.includes(moment(date).day()));
+        });
 };
