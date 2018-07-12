e7fe1ac59e67360afaf5a4214f05393cebcf733d
Bo Chen <bochen303@gmail.com>
2018-07-12 18:29:05 +0700

    remove list from update todo

--- src/questions/update_todo.js
+++ src/questions/update_todo.js
@@ -2,102 +2,93 @@
 
 const moment = require('moment');
 
-module.exports = function (todoItems) {
-    return [
-        {
-            type: 'list',
-            name: 'chosen_todo_item',
-            message: 'Which todo item do you want to modify?',
-            choices: todoItems,
-            pageSize: 10
-        },
-        {
-            type: 'list',
-            name: 'action',
-            message: 'What do you want to do to this item?',
-            choices: ['Complete', 'Punt', 'Cancel', 'Increment']
-        },
-        {
-            type: 'input',
-            name: 'increment_action',
-            message: 'What did you do to increment this item?',
-            validate: require('./validators/required'),
-            when: function (answers) {
-                return answers.action === 'Increment';
-            }
-        },
-        {
-            type: 'input',
-            name: 'increment_feeling',
-            message: 'How did you feel while incrementing this item?',
-            validate: require('./validators/required'),
-            when: function (answers) {
-                return answers.action === 'Increment';
-            }
-        },
-        {
-            type: 'input',
-            name: 'complete_action',
-            message: 'What did you do to complete this item?',
-            validate: require('./validators/required'),
-            when: function (answers) {
-                return answers.action === 'Complete';
-            }
-        },
-        {
-            type: 'input',
-            name: 'task_feeling',
-            message: 'How did you feel while doing this task?',
-            validate: require('./validators/required'),
-            when: function (answers) {
-                return answers.action === 'Complete';
-            }
-        },
-        {
-            type: 'input',
-            name: 'actual_duration',
-            message: 'How many minutes did this task take to complete?',
-            validate: require('./validators/positive_number'),
-            filter: Number,
-            when: function (answers) {
-                return answers.action === 'Complete';
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'should_follow_up',
-            message: 'Do you need to follow up and do something else after this?',
-            when: function (answers) {
-                return answers.action === 'Complete';
-            }
-        },
-        {
-            type: 'input',
-            name: 'punt_reason',
-            message: 'What is the reason you\'re punting this task?',
-            validate: require('./validators/required'),
-            when: function (answers) {
-                return answers.action === 'Punt'
-            }
-        },
-        {
-            type: 'datetime',
-            name: 'new_start_date',
-            message: 'What is the new start date you want to punt to?',
-            format: ['dddd', ' ', 'mm', '/', 'dd', '/', 'yyyy'],
-            when: function (answers) {
-                return answers.action === 'Punt'
-            },
-            initial: moment().add(1, 'day').toDate()
-        },
-        {
-            type: 'input',
-            name: 'cancellation_reason',
-            message: 'What is the reason you\'re cancelling this task?',
-            validate: require('./validators/required'),
-            when: function (answers) {
-                return answers.action === 'Cancel'
-            }
+module.exports = [
+    {
+        type: 'list',
+        name: 'action',
+        message: 'What do you want to do to this task?',
+        choices: ['Complete', 'Punt', 'Cancel', 'Increment']
+    },
+    {
+        type: 'input',
+        name: 'increment_action',
+        message: 'What did you do to increment this task?',
+        validate: require('./validators/required'),
+        when: function (answers) {
+            return answers.action === 'Increment';
         }
-    ];
-};
+    },
+    {
+        type: 'input',
+        name: 'increment_feeling',
+        message: 'How did you feel while incrementing this task?',
+        validate: require('./validators/required'),
+        when: function (answers) {
+            return answers.action === 'Increment';
+        }
+    },
+    {
+        type: 'input',
+        name: 'complete_action',
+        message: 'What did you do to complete this task?',
+        validate: require('./validators/required'),
+        when: function (answers) {
+            return answers.action === 'Complete';
+        }
+    },
+    {
+        type: 'input',
+        name: 'task_feeling',
+        message: 'How did you feel while doing this task?',
+        validate: require('./validators/required'),
+        when: function (answers) {
+            return answers.action === 'Complete';
+        }
+    },
+    {
+        type: 'input',
+        name: 'actual_duration',
+        message: 'How many minutes did this task take to complete?',
+        validate: require('./validators/positive_number'),
+        filter: Number,
+        when: function (answers) {
+            return answers.action === 'Complete';
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'should_follow_up',
+        message: 'Do you need to follow up and do something else after this?',
+        when: function (answers) {
+            return answers.action === 'Complete';
+        }
+    },
+    {
+        type: 'input',
+        name: 'punt_reason',
+        message: 'What is the reason you\'re punting this task?',
+        validate: require('./validators/required'),
+        when: function (answers) {
+            return answers.action === 'Punt'
+        }
+    },
+    {
+        type: 'datetime',
+        name: 'new_start_date',
+        message: 'What is the new start date you want to punt to?',
+        format: ['dddd', ' ', 'mm', '/', 'dd', '/', 'yyyy'],
+        when: function (answers) {
+            return answers.action === 'Punt'
+        },
+        initial: moment().add(1, 'day').toDate()
+    },
+    {
+        type: 'input',
+        name: 'cancellation_reason',
+        message: 'What is the reason you\'re cancelling this task?',
+        validate: require('./validators/required'),
+        when: function (answers) {
+            return answers.action === 'Cancel'
+        }
+    }
+];
--- src/update_todo.js
+++ src/update_todo.js
@@ -18,8 +18,6 @@ const inProgressTasks = require('./views/pending_tasks')(moment().toDate());
 inquirer.registerPrompt('datetime', require('inquirer-datepicker-prompt'));
 inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
 
-console.log('Lets update your todo items!');
-
 const updatedTasks = inProgressTasks.sort(
     (a, b) => { return (b.increment_count * -10000 + b.score) - (a.increment_count * -10000 + a.score); }
 ).slice(0, 1).map(t => {
@@ -29,12 +27,14 @@ const updatedTasks = inProgressTasks.sort(
     };
 });
 
-const todoQuestions = require('./questions/update_todo')(updatedTasks);
+const todoQuestions = require('./questions/update_todo');
 const taskQuestions = require('./questions/task')(responsibilities, values);
 
 if (inProgressTasks.length === 0) {
     console.log('You\'re all done for today! Go enjoy your life or add more tasks');
 } else {
+    console.log(`\n${updatedTasks[0].name}\n`);
+
     inquirer.prompt(todoQuestions).then(answers => {
         if (answers.action === 'Complete') {
             taskEvents.push({
@@ -42,7 +42,7 @@ if (inProgressTasks.length === 0) {
                 created: moment().toDate().toISOString(),
                 name: 'COMPLETE_TASK',
                 data: {
-                    chosen_todo_item: answers.chosen_todo_item,
+                    chosen_todo_item: updatedTasks[0].value,
                     complete_action: answers.complete_action,
                     task_feeling: answers.task_feeling,
                     actual_duration: answers.actual_duration
@@ -57,7 +57,7 @@ if (inProgressTasks.length === 0) {
                         created: moment().toDate().toISOString(),
                         data: {
                             ...taskAnswers,
-                            last_task_id: answers.chosen_todo_item
+                            last_task_id: updatedTasks[0].value
                         }
                     });
                 });
@@ -68,7 +68,7 @@ if (inProgressTasks.length === 0) {
                 created: moment().toDate().toISOString(),
                 name: 'PUNT_TASK',
                 data: {
-                    chosen_todo_item: answers.chosen_todo_item,
+                    chosen_todo_item: updatedTasks[0].value,
                     new_start_date: answers.new_start_date,
                     punt_reason: answers.punt_reason
                 }
@@ -79,7 +79,7 @@ if (inProgressTasks.length === 0) {
                 created: moment().toDate().toISOString(),
                 name: 'CANCEL_TASK',
                 data: {
-                    chosen_todo_item: answers.chosen_todo_item,
+                    chosen_todo_item: updatedTasks[0].value,
                     cancellation_reason: answers.cancellation_reason
                 }
             });
@@ -89,7 +89,7 @@ if (inProgressTasks.length === 0) {
                 created: moment().toDate().toISOString(),
                 name: 'INCREMENT_TASK',
                 data: {
-                    chosen_todo_item: answers.chosen_todo_item,
+                    chosen_todo_item: updatedTasks[0].value,
                     increment_action: answers.increment_action,
                     increment_feeling: answers.increment_feeling
                 }
