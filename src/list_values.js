980283edf1d2af509d2b9f477655c5f918ca3623
Bo Chen <bochen303@gmail.com>
2018-07-31 12:52:40 +0700

    add colors to values list

--- src/list_values.js
+++ src/list_values.js
@@ -2,7 +2,8 @@
 
 const valueEvents = require('../data/value_events.json');
 const values = valueEvents.reduce(require('./reducers/value'), []);
+const colors = require('colors');
 
-console.log(values.map(v => {
-    return v.name;
-}).join('\n'));
+console.log('\n' + values.map(v => {
+    return v.name.green;
+}).join('\n') + '\n');
