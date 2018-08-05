1b1fde713c3b45ced4d442038500093913cb6397
Bo Chen <bochen303@gmail.com>
2018-08-05 14:43:37 +0700

    add config for task score multipliers

--- src/views/scored_tasks.js
+++ src/views/scored_tasks.js
@@ -5,11 +5,13 @@ const _ = require('underscore');
 const tasks = require('./tasks');
 const responsibilities = require('./responsibilities');
 
+const config = require('../../config.json');
+
 const scoredTasks = tasks.map(t => {
     let responsibility = responsibilities.filter(r => r.id === t.responsibility)[0];
 
     t.responsibility_name = responsibility.name;
-    t.score = t.impact * 100 + responsibility.significance * 10 + t.urgency - t.punt_count * 10;
+    t.score = t.impact * config.task_score_multiplier_impact + responsibility.significance * config.task_score_multiplier_significance + t.urgency * config.task_score_multiplier_urgency + t.punt_count * config.task_score_multiplier_punt_count;
 
     return t;
 });
