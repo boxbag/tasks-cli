cf51d0e13b1c4962ba55ff995bd0020304225e04
Bo Chen <bochen303@gmail.com>
2018-08-05 16:00:50 +0700

    add config for recent finished tasks

--- src/views/recent_finished_tasks.js
+++ src/views/recent_finished_tasks.js
@@ -3,8 +3,9 @@
 const moment = require('moment');
 
 const completedTasks = require('./completed_tasks');
+const config = require('../../config');
 
 const recentFinishedTasks = completedTasks
-    .filter(t => moment(t.completed_date).startOf('day').toDate() >= moment().subtract(14, 'days').startOf('day').toDate());
+    .filter(t => moment(t.completed_date).startOf('day').toDate() >= moment().subtract(config.recent_finished_task_days, 'days').startOf('day').toDate());
 
 module.exports = recentFinishedTasks;
