b80a7c805a2b0386636cc041beb50d779eeea9fb
Bo Chen <bochen303@gmail.com>
2019-01-16 06:34:36 +0700

    hide create responsibility

--- src/next.js
+++ src/next.js
@@ -2,7 +2,6 @@
 
 const eventPublisher = require('./utils/event_publisher');
 
-const nextQuestionaire = require('./questions/next');
 const responsibilityQuestionaire = require('./questions/responsibility');
 const taskQuestionaire = require('./questions/task');
 
@@ -11,15 +10,7 @@ const newTaskPrinter = require('./printers/new_task');
 (async function () {
     newTaskPrinter();
 
-    const answers = await nextQuestionaire();
+    const taskAnswers = await taskQuestionaire();
 
-    if (answers.type === 'Responsibility') {
-        const responsibilityAnswers = await responsibilityQuestionaire();
-
-        eventPublisher('responsibility_events', 'CREATE_RESPONSIBILITY', responsibilityAnswers);
-    } else if (answers.type === 'Task') {
-        const taskAnswers = await taskQuestionaire();
-
-        eventPublisher('task_events', 'CREATE_TASK', taskAnswers);
-    }
+    eventPublisher('task_events', 'CREATE_TASK', taskAnswers);
 })();
