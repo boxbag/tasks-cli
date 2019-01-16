4a4258be60d732a179de783a98301fa2474f364b
Bo Chen <bochen303@gmail.com>
2019-01-17 06:31:56 +0700

    add next check date to increment so we can configure when we want to see the task again

--- src/questions/templates/increment_task.js
+++ src/questions/templates/increment_task.js
@@ -8,5 +8,12 @@ module.exports = [
         name: 'increment_action',
         message: 'What did you do to increment this task?',
         validate: require('../validators/required')
+    },
+    {
+        type: 'datetime',
+        name: 'next_check_date',
+        message: 'When do you want to check on this task again?',
+        format: ['dddd', ' ', 'mm', '/', 'dd', '/', 'yyyy'],
+        initial: moment().toDate()
     }
 ];
