f3d4c662b0fd62b2b9e0130336766b34552f31ab
Bo Chen <bochen303@gmail.com>
2019-01-17 05:27:47 +0700

    add printer for success metrics

--- src/printers/task_update.js
+++ src/printers/task_update.js
@@ -4,5 +4,6 @@ const colors = require('colors');
 
 module.exports = task => {
     console.log(`\n${colors.green(`${task.score} [${task.recurring_type}] (${task.recurring_count || 0}/${task.stop_recurrence_after || 0}) ${task.name}`)}\n`);
-    console.log(`${colors.yellow(task.necessary_reason)}\n`);
+    console.log(`${colors.bold.underline.white('Why?')} ${colors.yellow(task.necessary_reason)}\n`);
+    console.log(`${colors.bold.underline.white('Success measured by')} ${colors.yellow(task.success_metrics || 'not specified')}\n`);
 };
