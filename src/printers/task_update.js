a19c660cf747e60b23d3720f8f145d48e5f535b1
Bo Chen <bochen303@gmail.com>
2019-01-17 04:56:23 +0700

    remove usage of can delegate flag and reason cannot delegate

--- src/printers/task_update.js
+++ src/printers/task_update.js
@@ -5,10 +5,4 @@ const colors = require('colors');
 module.exports = task => {
     console.log(`\n${colors.green(`${task.score} [${task.recurring_type}] (${task.recurring_count || 0}/${task.stop_recurrence_after || 0}) ${task.name}`)}\n`);
     console.log(`${colors.yellow(task.necessary_reason)}\n`);
-
-    if (task.can_delegate) {
-        console.log(`Delegate: ${colors.cyan(task.delegate)}\n`);
-    } else {
-        console.log(`Delegate: ${colors.cyan('Consider pairing up on this task')}\n`);
-    }
 };
