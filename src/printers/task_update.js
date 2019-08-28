50321b8976d73174cda9548d077487ae7e87ebe1
Bo Chen <bochen303@gmail.com>
2019-08-28 10:27:33 +0700

    add previous increment steps when updating a task for additional context

--- src/printers/task_update.js
+++ src/printers/task_update.js
@@ -3,7 +3,15 @@
 const colors = require('colors');
 
 module.exports = task => {
-    console.log(`\n${colors.green(`${task.score} [${task.recurring_type}] (${task.recurring_count || 0}/${task.stop_recurrence_after || 0}) ${task.name}`)}\n`);
-    console.log(`${colors.bold.underline.white('Why?')} ${colors.yellow(task.necessary_reason)}\n`);
-    console.log(`${colors.bold.underline.white('Success measured by')} ${colors.yellow(task.success_metrics || 'not specified')}\n`);
+    console.log(`\n${colors.bold.green(`${task.score} [${task.recurring_type}] (${task.recurring_count || 0}/${task.stop_recurrence_after || 0}) ${task.name}`)}\n`);
+    console.log(`${colors.bold.underline.white('Why?')} ${colors.bold.yellow(task.necessary_reason)}\n`);
+    console.log(`${colors.bold.underline.white('Success measured by')} ${colors.bold.yellow(task.success_metrics || 'not specified')}\n`);
+
+    if (task.increment_actions && task.increment_actions.length > 0) {
+        let incrementActions = task.increment_actions.slice(0, 3).map(incrementAction => {
+            return `${colors.bold.underline.white(new Date(incrementAction.date).toLocaleDateString('en-us'))} - ${colors.bold.yellow(incrementAction.action)}`;
+        }).join('\n');
+
+        console.log(`${incrementActions}\n`);
+    }
 };
